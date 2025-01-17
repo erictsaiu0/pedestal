a48b6776803a6e2ea93f408ca5bd67e1cff3ad14
erictsaiu0 <knighterictsai@gmail.com>
2025-01-17 20:10:10 +0800

    minor update

--- server_setup.sh
+++ server_setup.sh
@@ -22,6 +22,7 @@ SCRIPT_B_PATH = "server_activate.sh"
 USER_NAME=$(whoami)
 BASHRC_PATH="/home/$USER_NAME/.bashrc"
 
+echo "SCRIPT_B_PATH: $SCRIPT_B_PATH"
 # 將指令加入 .bashrc
 EXEC_CMD="cd $(pwd) && screen -L -dmS server bash ./$SCRIPT_B_PATH --zoom $ZOOM --text_num $TEXT_NUM --audio_playlist $AUDIO_PLAYLIST --audio_detach $AUDIO_DETACH --high_sync $HIGH_SYNC --detect_interval $DETECT_INTERVAL"
 
